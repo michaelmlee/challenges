0a7435eca8339f26694cd06404fcf72a52e29c26
Michael M. Lee <michmlee92@gmail.com>
2020-06-04 16:34:11 -0700

    added new solution from coding interview with unit tests

---
+++ js_solutions/columnName.js
@@ -0,0 +1,40 @@
+function printColName(input){
+    const alphabet = ["Z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y"];
+    let i = input;
+    let colNameArray = [];
+    console.log(i);
+    while (i > 0){
+        const remainder = i % 26;
+        console.log(i);
+        colNameArray.unshift(alphabet[remainder]);
+        i = remainder === 0 ? Math.floor(i / 26) - 1 : Math.floor(i / 26);
+    }
+    return colNameArray.join("");
+}
+
+function main(){
+    //uncomment to test
+    // console.log(printColName(26)); //Z
+    // console.log(printColName(27)); //AA
+    // console.log(printColName(51)); //AY
+    // console.log(printColName(52)); //AZ  
+    // console.log(printColName(80)); //CB
+    // console.log(printColName(676)); //YZ
+    // console.log(printColName(702)); // ZZ
+    // console.log(printColName(703)); // AAA
+    // console.log(printColName(705)); //AAC
+    // console.log(printColName(1406)); //BBB
+    // console.log(printColName(88888)); //EALT
+    // console.log(printColName(145145)); //HFRM
+    // console.log(printColName(234899)); //MILO
+    // console.log(printColName(234863)); // MIKE
+    // console.log(printColName(10000000)); //UVXWJ
+    // console.log(printColName(48151623)); //OOFZIEP
+}
+
+//for testing
+main();
+
+module.exports = {
+    printColName
+};
---
+++ js_solutions/columnName.test.js
@@ -0,0 +1,66 @@
+const { printColName } = require('./columnName');
+
+describe("Print Column Name", () => {
+    test("1", () => {
+        expect(printColName(1)).toBe('A');
+    });
+
+    test("26", () => {
+        expect(printColName(26)).toBe('Z');
+    });
+
+    test("27", () => {
+        expect(printColName(27)).toBe('AA');
+    });
+
+    test("51", () => {
+        expect(printColName(51)).toBe('AY');
+    });
+
+    test("52", () => {
+        expect(printColName(52)).toBe('AZ');
+    });
+
+    test("80", () => {
+        expect(printColName(80)).toBe('CB');
+    });
+
+    test("676", () => {
+        expect(printColName(676)).toBe('YZ');
+    });
+
+    test("702", () => {
+        expect(printColName(702)).toBe('ZZ');
+    });
+
+    test("703", () => {
+        expect(printColName(703)).toBe('AAA');
+    });
+
+    test("705", () => {
+        expect(printColName(705)).toBe('AAC');
+    });
+
+    test("1406", () => {
+        expect(printColName(1406)).toBe('BBB');
+    });
+
+    test("88888", () => {
+        expect(printColName(88888)).toBe('EALT');
+    });
+
+    test("234863", () => {
+        expect(printColName(234863)).toBe('MIKE');
+    });
+
+    test("10000000", () => {
+        expect(printColName(10000000)).toBe('UVXWJ');
+    });
+
+    test("4815162342", () => {
+        expect(printColName(4815162342)).toBe('OOFZIEP');
+    });
+
+
+});
+
